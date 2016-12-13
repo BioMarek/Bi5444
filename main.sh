34dc8c66aac8f5f1912132bb8401de74f4b6724a
indrakovaa <indrakova.a@gmail.com>
2016-12-13 11:44:04 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -186,12 +186,14 @@ cd $DATASET_DIR # Go to the input directory
 for sample in *$INPUT_SUFFIX # For each file with specified suffix in the directory do the pre-processing loop
 do
 	# Cutadapt adapter removal, quality trimming, N bases removal  and length filtering
+	# for quality filtering we need Phred coding +33, otherwise --quality-base=$QUALITY
 	# --untrimmed-output=$OUTPUT_DIR/${sample%.fastq*}.ad3untrimmed.fastq.gz \
 	# we do not have adapters, we do only size and quality filtering, so we cannot use --untrimmed-output =Write all reads without adapters to FILE (in FASTA/FASTQ format) instead of writing them to the regular output file.
 	cutadapt --quality-cutoff $QT_THRESHOLD,$QT_THRESHOLD --trim-n --max-n=0 \
 	--minimum-length $DISC_SHORT --maximum-length $DISC_LONG -o $OUTPUT_DIR/${sample%.fastq*}.ad3trim.fastq.gz $sample #cuadapt detects format automatically
 
 	# Fastx-toolkit quality filtering; to use gz as input/output https://www.biostars.org/p/83237/
+	# Cutadapt can trim only ends of the reads. To filter sequences with low qualitys in the middle, we need to use FastX
 	gunzip -c $OUTPUT_DIR/${sample%.fastq*}.ad3trim.fastq.gz | fastq_quality_filter -Q $QUALITY \
 	-q $QF_THRESHOLD -p $QF_PERC -z -o $OUTPUT_DIR/${sample%.fastq*}.mirna.fastq.gz
 done
