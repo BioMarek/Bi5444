9d18d3644593e76db6645ac0ebad8c0bb4db814b
BioMarek <BioMarek@users.noreply.github.com>
2017-01-08 09:32:58 +0100

    Create fastqc_analysis.sh

---
+++ fastqc_analysis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Following script performs quality check using FastQC.
+# for loop copying everything to $SCRATCH
+cd $PROJECT_DIR
+mkdir fastqc_before_trim
+cd $PROJECT_DIR/raw_sequences
+
+for file in *
+do
+  echo copying "$file" # tells us which file is being copied, just to know where we are
+  cp $file $SCRATCH/$file
+done
+
+# FastQC analysis
+cd $SCRATCH
+module add fastQC-0.10.1
+for file in *
+do
+  fastqc $file
+done
+
+mv *.zip $PROJECT_DIR/fastqc_before_trim/ # copies results to our storage directory
