274758c3983ee5d4c0af242d928af01d9471f4a3
BioMarek <BioMarek@users.noreply.github.com>
2017-01-24 12:54:33 +0100

    Update adapter_searching.sh

--- adapter_searching.sh
+++ adapter_searching.sh
@@ -1,5 +1,3 @@
-### TODO do it in scratch and check whether it works
-
 # Simple script for minion adapter search
 # within the folder  (DATASET_DIR) it takes all files with
 # specified suffix (SUFFIX) and looks for the adapters and compares
@@ -15,9 +13,9 @@ DATASET_DIR=$PROJECT_DIR/raw_sequences
 OUTPUT_DIR=$PROJECT_DIR/minion
 
 # Downloading of adapters, list of possible adapters is stored in study materials
-cd $SCRATCH
+cd $PROJECT_DIR
 wget https://is.muni.cz/el/1431/podzim2016/Bi5444/um/65638858/adapters_merge.txt
-ADAPTERS=$SCRATCH/adapters_merge.txt
+ADAPTERS=$PROJECT_DIR/adapters_merge.txt
 
 # Downloading and instalation of minion nad swan
 wget http://wwwdev.ebi.ac.uk/enright-dev/kraken/reaper/src/reaper-15-065.tgz 
@@ -33,7 +31,7 @@ cd $DATASET_DIR
 for i in *
 do	
 	# Identify top 5 over-represented sequences
-	$SCRATCH/reaper-15-065/src/minion search-adapter -i $i -show 5 -write-fasta $OUTPUT_DIR/${i%.*}.minion.fasta
+	$PROJECT_DIR/reaper-15-065/src/minion search-adapter -i $i -show 5 -write-fasta $OUTPUT_DIR/${i%.*}.minion.fasta
 	# Compare them with list of adapters
-	$SCRATCH/reaper-15-065/src/swan -r $ADAPTERS -q $OUTPUT_DIR/${i%.*}.minion.fasta > $OUTPUT_DIR/${i%.*}.minion.compare 
+	$PROJECT_DIR/reaper-15-065/src/swan -r $ADAPTERS -q $OUTPUT_DIR/${i%.*}.minion.fasta > $OUTPUT_DIR/${i%.*}.minion.compare 
 done
