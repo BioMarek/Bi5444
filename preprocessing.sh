7dae075b2469181781b59965b231e5a6886625ea
BioMarek <BioMarek@users.noreply.github.com>
2017-01-25 09:16:24 +0100

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -20,8 +20,8 @@ OUTPUT_DIR=$PROJECT_DIR/trimming #path to output seqquences
 FILE_FORMAT=fastq # File format
 QUALITY=33 # Phred coding of input files
 
-### What kind of coding is used for Phred? ##
-##head -n 40 $datafile | awk '{if(NR%4==0) printf("%s",$0);}' |  od -A n -t u1 | awk 'BEGIN{min=100;max=0;}{for(i=1;i<=NF;i++) {if($i>max) max=$i; if($i<min) min=$i;}}END{if(max<=74 && min<59) print "Phred+33"; else if(max>73 && min>=64) print "Phred+64"; else if(min>=59 && min<64 && max>73) print "Solexa+64"; else print "Unknown score encoding\!";}'
+## Following line of code detects quality encoding (Phred+33, Phred+64, Solexa+64)
+# head -n 40 $datafile | awk '{if(NR%4==0) printf("%s",$0);}' |  od -A n -t u1 | awk 'BEGIN{min=100;max=0;}{for(i=1;i<=NF;i++) {if($i>max) max=$i; if($i<min) min=$i;}}END{if(max<=74 && min<59) print "Phred+33"; else if(max>73 && min>=64) print "Phred+64"; else if(min>=59 && min<64 && max>73) print "Solexa+64"; else print "Unknown score encoding\!";}'
 
 
 ##CUTADAPT VARIABLES#
