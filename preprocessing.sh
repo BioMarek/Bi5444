2bfed13c3fc58305f48e66fd4b27e2d2c010dc5d
BioMarek <BioMarek@users.noreply.github.com>
2017-02-02 18:31:59 +0100

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,62 +1,59 @@
 #!/bin/bash
 #
-###################################################################################################
+##############################################################################################################################
 ###INFORMATION ABOUT THE SCRIPT###
 # Pre-processing script designed for microRNAs analysis using Cutadapt (http://cutadapt.readthedocs.io/en/stable/index.html) 
-# and Fastx-toolkit (http://hannonlab.cshl.edu/fastx_toolkit/)
-# It is focused for later use of Chimira (http://www.ebi.ac.uk/research/enright/software/chimira) (current version: http://wwwdev.ebi.ac.uk/enright-dev/chimira/)
-# and DESeq2 (https://bioconductor.org/packages/release/bioc/html/DESeq2.html) later on
+# and Fastx-toolkit (http://hannonlab.cshl.edu/fastx_toolkit/) It is focused for later use of Chimira 
+# (http://www.ebi.ac.uk/research/enright/software/chimira) and DESeq2 
+# (https://bioconductor.org/packages/release/bioc/html/DESeq2.html) later on
 #
-# The script does following steps in order to preprocess data for mapping
+# The script does following steps in order to preprocess data for mapping:
 # 1) Adapter trimming
 # 2) Quality trimming
 # 3) Size filtering
 # 4) Quality filtering 
 
-###################################################################################################
+##############################################################################################################################
 ##SPECIFY DATA VARIABLES###
 PROJECT_DIR=/storage/brno2/home/marek_bfu/Bi5444
 INPUT_SUFFIX=".fastq.gz" # Suffix of files to launch the analysis on
-DATASET_DIR=$PROJECT_DIR/raw_sequences #path to input raw sequences
-OUTPUT_DIR=$PROJECT_DIR/trimming #path to output sequences
+DATASET_DIR=$PROJECT_DIR/raw_sequences # path to input raw sequences
+OUTPUT_DIR=$PROJECT_DIR/trimming # path to output sequences
 
-FILE_FORMAT=fastq # File format
-QUALITY=33 # Phred coding of input files
-
-## Following line of code detects quality encoding (Phred+33, Phred+64, Solexa+64)
+## Following line of code detects quality encoding (Phred+33, Phred+64, Solexa+64), uncomment if you don't know your encoding
 # head -n 40 $datafile | awk '{if(NR%4==0) printf("%s",$0);}' |  od -A n -t u1 | awk 'BEGIN{min=100;max=0;}{for(i=1;i<=NF;i++) {if($i>max) max=$i; if($i<min) min=$i;}}END{if(max<=74 && min<59) print "Phred+33"; else if(max>73 && min>=64) print "Phred+64"; else if(min>=59 && min<64 && max>73) print "Solexa+64"; else print "Unknown score encoding\!";}'
 
+FILE_FORMAT=fastq # File format
+QUALITY=33 # Phred coding of input files, change if you have something else
 
-##CUTADAPT VARIABLES#
+###CUTADAPT VARIABLES###
 QT_THRESHOLD=5 # Threshold for quality trimming; we filter by number of mismatches so we need high quality reads
 
-#selecting sequences that have lenght of miRNAs 
+# selecting sequences that have lenght of miRNAs 
 DISC_SHORT=15 # Discard too short sequences after the pre-processing
 DISC_LONG=26 # Discard too long after the pre-processing
 
-
-
-##FASTX - QUALITY FILTERING VARIABLES###
+###FASTX - QUALITY FILTERING VARIABLES###
 QF_THRESHOLD=10 # Threshold for quality filtering
 QF_PERC=85 # Minimal percentage of bases with $QF_THRESHOLD
 
-##Add modules
-
-#add Cudadapt module
+###ADD MODULES###
+# add Cudadapt module
 module add python27-modules-gcc  
-module add python27-modules-intel\
-#add FastX toolkit
+module add python27-modules-intel
+
+# add FastX toolkit
 module add fastx-0.0.13 
 
-###################################################################################################
+##############################################################################################################################
 ###SCRIPT BODY###
-mkdir -p $OUTPUT_DIR # Create output directory including tmp folder for additional results
+mkdir -p $OUTPUT_DIR
 
-cd $DATASET_DIR # Go to the input directory
+cd $DATASET_DIR
 
 for sample in *$INPUT_SUFFIX # For each file with specified suffix in the directory do the pre-processing loop
 do
-	# Cutadapt adapter removal, quality trimming, N bases removal  and length filtering
+	# Cutadapt adapter removal, quality trimming, N bases removal and length filtering
 	# for quality filtering we need Phred coding +33, otherwise --quality-base=$QUALITY
 	# --untrimmed-output=$OUTPUT_DIR/${sample%.fastq*}.ad3untrimmed.fastq.gz \
 	# we do not have adapters, we do only size and quality filtering, so we cannot use --untrimmed-output =Write all reads without adapters to FILE (in FASTA/FASTQ format) instead of writing them to the regular output file.
