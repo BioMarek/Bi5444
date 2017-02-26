a838714d9e42909594890c066ee9b48c02e36d23
BioMarek <BioMarek@users.noreply.github.com>
2017-02-26 19:25:53 +0100

    Create counting .sh

---
+++ counting .sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#######################################################################################################################
+###INFORMATION ABOUT THE SCRIPT###
+# The script counts length distribution of sequences so we can make histogram
+
+#######################################################################################################################
+##SPECIFY DATA VARIABLES###
+PROJECT_DIR=/storage/brno2/home/marek_bfu/Bi5444 # path to project dir
+DATASET_DIR=$PROJECT_DIR/trimming # path to input filtered sequences
+OUTPUT_DIR=$PROJECT_DIR/counts # path to output sequences
+
+#######################################################################################################################
+###SCRIPT BODY###
+mkdir $OUTPUT_DIR
+cd $DATASET_DIR
+
+for file in *mirna.fastq.gz
+do
+   # leaves sequences from fastq file, removes everything else. Guzip unpacks the file and sends output on STDIN; 
+   # sed removes all lines except nucleotide sequence; awk prints lengths of sequencs; uniq counts the sequences.
+   # sort is there because uniq works on adjacent matching lines (see manual)
+   gunzip -c $file | sed -n 'n;p;n;n;' | awk '{print length}' | sort | uniq -c > $OUTPUT_DIR/${file:0:9}_counts.txt
+done
