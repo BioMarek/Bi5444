d05679e5a225856d4592ecadd0111a1f922ac2ed
BioMarek <BioMarek@users.noreply.github.com>
2017-03-06 14:23:39 +0100

    Update counting.sh

--- counting.sh
+++ counting.sh
@@ -7,7 +7,7 @@
 #######################################################################################################################
 ###SPECIFY DATA VARIABLES###
 # Change PROJECT_DIR variable to your favorite storage. Results from further analysis
-#  steps will be stored here.
+# steps will be stored here.
 PROJECT_DIR=/storage/brno2/home/marek_bfu/Bi5444 # path to project dir
 DATASET_DIR=$PROJECT_DIR/trimming # path to input filtered sequences
 OUTPUT_DIR=$PROJECT_DIR/counts # path to output sequences
@@ -21,7 +21,7 @@ for file in *mirna.fastq.gz
 do
    # The script leaves only sequences in fastq file, removes everything else. And then calculates length distribution.
    # Gunzip unpacks the file and sends output on STDOUT; 
-   # sed removes all lines except nucleotide sequence; awk prints lengths of sequencs; uniq counts the sequencesl;
+   # sed removes all lines except nucleotide sequence; awk prints lengths of sequencs; uniq counts the lengths;
    # sort is there because uniq works on adjacent matching lines (see manual)
    gunzip -c $file | sed -n 'n;p;n;n;' | awk '{print length}' | sort | uniq -c > $OUTPUT_DIR/${file:0:9}_counts.txt
 done
